6089f78c6ce9f0e060498162c704c60ecfd17812
numzero <numzer0@yandex.ru>
2023-06-15 23:29:33 +0300

    Irrlichtless builds

---
+++ scripts/JPEG.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+jpeg_ver=2.1.4
+
+download () {
+	get_tar_archive libjpeg "https://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-${jpeg_ver}.tar.gz"
+}
+
+build () {
+	cmake "${CMAKE_FLAGS[@]}" \
+		-DCMAKE_INSTALL_PREFIX=/usr/local \
+		-DENABLE_SHARED=OFF \
+		-DWITH_TURBOJPEG=OFF \
+		$srcdir/libjpeg
+	make
+	make_install_copy
+}
